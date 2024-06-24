4410d40d20c34da8e9ac5ccc01d469d741e519de
Zs01t <bekeffy1@gmail.com>
2024-06-24 15:40:22 +0200

    Physics Component and gravity is impleneted, also can move player

--- pixelholdersPlatformer/classes/Component/PhysicsComponent.cs
+++ pixelholdersPlatformer/classes/Component/PhysicsComponent.cs
@@ -1,3 +1,63 @@
-﻿namespace pixelholdersPlatformer.classes.Component;
+﻿using pixelholdersPlatformer.classes.gameObjects;
+using System.Numerics;
 
-public class PhysicsComponent : IComponent{};
+namespace pixelholdersPlatformer.classes.Component;
+
+public class PhysicsComponent : IComponent
+{
+
+    private GameObject _owner;
+
+    private const double _gravity = 9.8d;
+
+    private Vector2 _velocity;
+
+    public PhysicsComponent(GameObject owner)
+    {
+        _owner = owner;
+    }
+
+    public void SetVelocity(float amountX, float amountY)
+    {
+        _velocity.X = amountX;
+        _velocity.Y = amountY;
+
+    }
+
+    public void SetVelocityX(float amountX)
+    {
+        _velocity.X = amountX;
+
+
+    }
+    public void SetVelocityY(float amountY)
+    {
+        _velocity.Y = amountY;
+
+    }
+
+    bool isColliding = true;
+    public void Update()
+    {
+        if (_owner.CoordY >= 100 - _owner.Height)
+        {
+            _owner.CoordY = 100 - _owner.Height;
+            if (_velocity.Y > 0 ) 
+            {
+                _velocity.Y = 0;
+            }
+            
+            
+        }
+        else
+        {
+            //TODO: not make it frame dependent
+            _velocity.Y += (float)_gravity * 0.016f;
+        }
+        
+
+        ((MovableComponent)_owner.Components.Where(t => t.GetType().Name == "MovableComponent").First()).MoveGameObject(_velocity.X, _velocity.Y);
+        SetVelocityX(0);
+
+    }
+};
