0fb5b80c2d7531679f7fe5b171f3b85cc04dea15
bekeffy.zsolt@stud.uni-obuda.hu <bekeffy.zsolt@stud.uni-obuda.hu>
2024-07-20 15:39:22 +0200

    enemy class created

---
+++ pixelholdersPlatformer/classes/gameObjects/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using pixelholdersPlatformer.classes.Component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pixelholdersPlatformer.classes.gameObjects
+{
+    public class Enemy : GameObject
+    {
+        private string _enemyType = "03-Pig";
+
+        public Enemy(float coordX, float coordY, float width, float height) : base(coordX, coordY, width, height)
+        {
+            this.Components.Add(new MovableComponent(this));
+            this.Components.Add(new PhysicsComponent(this));
+            this.Components.Add(new CollisionComponent(this));
+            this.Components.Add(new AnimatableComponent(this, _enemyType));
+        }
+
+        public void SetEnemyType(string enemyType)
+        { 
+            _enemyType = enemyType;
+        }
+
+    }
+
+}
