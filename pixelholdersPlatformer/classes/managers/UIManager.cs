502b2e5a3a3cd034553ef44675a7d9eb5ccb0ee1
xsilvermc <57896691+xsilvermc@users.noreply.github.com>
2024-07-25 18:14:31 +0200

    start implementation of UI

--- pixelholdersPlatformer/classes/managers/UIManager.cs
+++ pixelholdersPlatformer/classes/managers/UIManager.cs
@@ -1,6 +1,15 @@
-﻿namespace pixelholdersPlatformer.classes.managers;
+﻿using static SDL2.SDL;
+using static SDL2.SDL_ttf;
+
+namespace pixelholdersPlatformer.classes.managers;
 
 public class UIManager
 {
-    
+    private const String _fontPath = "assets/fonts/slkscr.ttf";
+    private nint _font = TTF_OpenFont(_fontPath, 12);
+
+    public UIManager()
+    {
+
+    }
 }
